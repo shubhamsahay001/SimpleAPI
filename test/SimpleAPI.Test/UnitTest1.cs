357a03a5db6bfb6c4362d273d82da1a78858f2ef
Shubham Sahay <shubham.sahay@sap.com>
2022-05-26 12:49:09 +0530

    Improved Unit tests

--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -15,9 +15,30 @@ public class UnitTest1
     }
 
     [Fact]
-       public void GetReturnsDataWithParam()
+       public void GetReturnRanchiData()
     {
         var returnValue = controller.Get("Ranchi");
         Assert.Equal("The average temparature in Ranchi will be 30.5 degree celsius in this week", returnValue);
+    }
+
+    [Fact]
+       public void GetReturnBangaloreData()
+    {
+        var returnValue = controller.Get("Bangalore");
+        Assert.Equal("The average temparature in Bangalore will be 25.5 degree celsius in this week", returnValue);
+    }
+
+    [Fact]
+       public void GetReturnBengaluruData()
+    {
+        var returnValue = controller.Get("Bengaluru");
+        Assert.Equal("The average temparature in Bengaluru will be 25.5 degree celsius in this week", returnValue);
+    }
+
+    [Fact]
+       public void GetReturnRandomData()
+    {
+        var returnValue = controller.Get("Random");
+        Assert.Equal("Sorry we don't have the weather forecast data of Random in our servers yet.", returnValue);
     }
 }
